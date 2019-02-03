31332ae0c92614910066cf0d5e2fd331a9bfa4f8
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-02-03 18:00:26 +0800

    add runCNVkit

--- runCNV.go
+++ runCNV.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/liserjrqlxue/simple-util"
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 )
 
 // os
@@ -33,8 +35,18 @@ var (
 	)
 	run2 = flag.String(
 		"run2",
-		exPath+pSep+"run.CNVkit.sh",
+		"/share/backup/hanrui/pipeline/CNVkit/bin/run_CNVkit.pl",
 		"CNVkit script to run",
+	)
+	CNVkitControl = flag.String(
+		"control",
+		"/share/backup/hanrui/pipeline/CNVkit/control/MGISEQ_2000_control/201811/MGISEQ-2000_201811",
+		"control of CNVkit",
+	)
+	submit = flag.Bool(
+		"submit",
+		false,
+		"if auto submit",
 	)
 )
 
@@ -47,6 +59,33 @@ func main() {
 	if *outdir == "" {
 		*outdir = path.Base(*indir)
 	}
+
 	simple_util.RunCmd(*run1, *indir, *outdir)
-	simple_util.RunCmd(*run2, *indir, *outdir)
+	runCNVkit(*run2, *indir, *outdir, *CNVkitControl, *submit)
+}
+
+func runCNVkit(script, indir, outdir, control string, submit bool) {
+	tag := path.Base(indir)
+	var args []string
+	args = append(args, script)
+	args = append(args, strings.Join([]string{outdir, "ExomeDepth", "sample.list.checked"}, pSep))
+	args = append(args, control)
+	args = append(args, strings.Join([]string{outdir, "CNVkit"}, pSep))
+	fmt.Printf("# perl %s\n", strings.Join(args, " "))
+	simple_util.RunCmd("perl", args...)
+
+	var args2 []string
+	args2 = append(args2,
+		"-cwd",
+		"-l", "vf=31G,p=12",
+		"-P", "B2C_SGD",
+		"-N", "CNVkit."+tag,
+		outdir+"/CNVkit/run.sh",
+	)
+	if submit {
+		simple_util.RunCmd("qsub", args2...)
+		fmt.Printf("# qsub %s\n", strings.Join(args2, " "))
+	} else {
+		fmt.Printf("# submit cmd:\nqsub %s\n", strings.Join(args2, " "))
+	}
 }
