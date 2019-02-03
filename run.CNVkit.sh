fc4b7f47a74188bd6a34bd501bf1b846e54ca241
liufengxia <liufengxia@genomics.cn>
2019-02-03 15:31:35 +0800

    change vf of CNVkit to 30G

--- run.CNVkit.sh
+++ run.CNVkit.sh
@@ -6,4 +6,4 @@ date=$(date --rfc-3339=date)
 echo "# perl /share/backup/hanrui/pipeline/CNVkit/bin/run_CNVkit.pl $prefix/ExomeDepth/sample.list.checked /share/backup/hanrui/pipeline/CNVkit/control/MGISEQ_2000_control/201811/MGISEQ-2000_201811 $prefix/CNVkit"
 perl /share/backup/hanrui/pipeline/CNVkit/bin/run_CNVkit.pl $prefix/ExomeDepth/sample.list.checked /share/backup/hanrui/pipeline/CNVkit/control/MGISEQ_2000_control/201811/MGISEQ-2000_201811 $prefix/CNVkit
 echo "# submit cmd:"
-echo qsub -cwd -l vf=4G,p=12 -P B2C_SGD -N CNVkit.$tag $prefix/CNVkit/run.sh
+echo qsub -cwd -l vf=30G,p=12 -P B2C_SGD -N CNVkit.$tag $prefix/CNVkit/run.sh
