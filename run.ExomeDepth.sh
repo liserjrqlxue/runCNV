3605734001cbeb1634b04138886765245cad7ce0
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-02-13 16:36:43 +0800

    rm run.ExomeDepth.sh

--- run.ExomeDepth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-path=$1
-tag=$(basename $path)
-prefix=$2
-date=$(date --rfc-3339=date)
-echo "# perl /share/backup/wangyaoshen/src/ExomeDepth/createScript.sgd.pl $path $prefix/ExomeDepth $tag"
-perl /share/backup/wangyaoshen/src/ExomeDepth/createScript.sgd.pl $path $prefix/ExomeDepth $tag
