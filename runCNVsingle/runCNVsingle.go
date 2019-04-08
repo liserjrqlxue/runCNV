adfcb67e92a6e024d15ed5c7c81c67d79a775f84
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-04-08 13:40:19 +0800

    add SMA for single sample

--- runCNVsingle/runCNVsingle.go
+++ runCNVsingle/runCNVsingle.go
@@ -32,7 +32,7 @@ var (
 	gender = flag.String(
 		"gender",
 		"",
-		"gender of sampleID",
+		"gender of sampleID,[F/M]",
 	)
 	run1 = flag.String(
 		"run1",
@@ -44,6 +44,11 @@ var (
 		"/ifs7/B2C_SGD/PROJECT/PP12_Project/wangyaoshen/CNVkit/bin/run_CNVkit.single.pl",
 		"CNVkit script to run single",
 	)
+	run3 = flag.String(
+		"run3",
+		"/ifs7/B2C_SGD/PROJECT/PP12_Project/wangyaoshen/SMA_WES/run_SMN_CNV_v2.single.pl",
+		"SMA script to run single sample",
+	)
 	ExomeDepthControl = flag.String(
 		"rds",
 		"/ifs7/B2C_SGD/PROJECT/PP12_Project/ExomeDepth/test/all",
@@ -53,6 +58,16 @@ var (
 		"control",
 		"/share/backup/hanrui/pipeline/CNVkit/control/MGISEQ_2000_control/201811/MGISEQ-2000_201811",
 		"control of CNVkit",
+	)
+	SMAControl = flag.String(
+		"smn",
+		"/ifs7/B2C_SGD/PROJECT/PP12_Project/ExomeDepth/workspace/SMA_WES/SMA_v2.txt.control_gene.csv",
+		"control of SMA",
+	)
+	SMAGene = flag.String(
+		"geneinfo",
+		"/ifs9/BC_PS/hanrui/pipeline/SMA_WES/PP100.gene.info.bed",
+		"gene info for SMA",
 	)
 	submit = flag.Bool(
 		"submit",
@@ -70,7 +85,7 @@ func main() {
 
 	runExomeDepth(*run1, *sampleID, *gender, *bam, *outdir, *ExomeDepthControl, *submit)
 	runCNVkit(*run2, *CNVkitControl, *outdir, *sampleID, *bam, *submit)
-
+	runSMA(*run3, *bam, *SMAGene, *SMAControl, *outdir, *sampleID, *submit)
 }
 
 // outdir/tag/sampleID/run.sh
@@ -112,6 +127,31 @@ func runCNVkit(script, control, outdir, sampleID, bam string, submit bool) {
 		args2,
 		"-cwd",
 		"-l", "vf=12G,p=12G",
+		"-P", "B2C_SGD",
+		"-N", tag+"."+sampleID,
+		strings.Join([]string{outdir, tag, sampleID, "run.sh"}, pSep),
+	)
+
+	if submit {
+		fmt.Printf("# qsub %s\n", strings.Join(args2, " "))
+		simple_util.RunCmd("qsub", args2...)
+	} else {
+		fmt.Printf("# submit cmd:\nqsub %s\n", strings.Join(args2, " "))
+	}
+}
+
+func runSMA(script, bam, geneInfo, control, outdir, sampleID string, submit bool) {
+	tag := "SMA"
+	var args []string
+	args = append(args, script, bam, geneInfo, control, outdir+pSep+tag)
+	fmt.Printf("# perl %s\n", strings.Join(args, " "))
+	simple_util.RunCmd("perl", args...)
+
+	var args2 []string
+	args2 = append(
+		args2,
+		"-cwd",
+		"-l", "vf=10G,p=1",
 		"-P", "B2C_SGD",
 		"-N", tag+"."+sampleID,
 		strings.Join([]string{outdir, tag, sampleID, "run.sh"}, pSep),
